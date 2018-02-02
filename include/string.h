8efdd509e8385e347a96a00e1db3320b46a2b552
Martin Swany <swany@iu.edu>
2018-02-02 17:48:36 -0500

    added prototype for strcmp

--- include/string.h
+++ include/string.h
@@ -3,6 +3,7 @@
 extern	char	*strncpy(char *, const char *, int32);
 extern	char	*strncat(char *, const char *, int32);
 extern	int32	strncmp(const char *, const char *, int32);
+extern	int	strcmp(char *, char *);
 extern	char	*strchr(const char *, int32);
 extern	char	*strrchr(const char *, int32);
 extern	char	*strstr(const char *, const char *);
