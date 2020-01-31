8bdb296ada43fb14e10feecd2dfdb6155c05bad1
Devansh Jain <devajain@iu.edu>
2020-01-31 11:46:01 -0500

    withoutsema

--- include/prodcons.h
+++ include/prodcons.h
@@ -1,6 +1,6 @@
 /*Global variable for producer consumer*/
 extern int n; /*this is just declaration*/
-extern sid32 prod, cons;
+//extern sid32 prod, cons;
 
 /*function Prototype*/
 void consumer(int count);
